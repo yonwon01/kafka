3c560e4f0f23d8c39d05516b4031855ad06abe24
seowon <yonwon01@naver.com>
2017-12-06 16:22:20 +0900

    init commit

---
+++ src/main/java/com/bigdata2017/kafka/ProducerExample.java
@@ -0,0 +1,23 @@
+package com.bigdata2017.kafka;
+
+import java.util.Properties;
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
+
+public class ProducerExample {
+
+	public static void main(String[] args) throws Exception {
+		Properties props = new Properties();
+		props.put("metadata.broker.list",
+				"redis.poptok.com:9092,kafka-test-002.epicdevs.com:9092,kafka-test-003.epicdevs.com:9092");
+		props.put("serializer.class", "kafka.serializer.StringEncoder");
+		ProducerConfig producerConfig = new ProducerConfig(props);
+		Producer<String, String> producer = new Producer<String, String>(producerConfig);
+		KeyedMessage<String, String> message = new KeyedMessage<String, String>("test", "Hello, World!");
+		producer.send(message);
+		producer.close();
+
+	}
+
+}
