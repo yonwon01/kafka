7b18ad2400b8719180acaa0954dcd23674149162
seowon <yonwon01@naver.com>
2017-12-08 14:30:41 +0900

    poducer change

--- src/main/java/com/bigdata2017/kafka/ProducerExample.java
+++ src/main/java/com/bigdata2017/kafka/ProducerExample.java
@@ -9,13 +9,14 @@ public class ProducerExample {
 
 	public static void main(String[] args) throws Exception {
 		Properties props = new Properties();
-		props.put("metadata.broker.list",
-				"redis.poptok.com:9092");
+		props.put("metadata.broker.list", "redis.poptok.com:9092");
 		props.put("serializer.class", "kafka.serializer.StringEncoder");
 		ProducerConfig producerConfig = new ProducerConfig(props);
 		Producer<String, String> producer = new Producer<String, String>(producerConfig);
-		KeyedMessage<String, String> message = new KeyedMessage<String, String>("test", "Hello, World!");
-		producer.send(message);
+		String hashtag="hashtag!";
+		KeyedMessage<String, String> message = new KeyedMessage<String, String>("test", hashtag);
+		for(int i=0;i<5;i++) {
+		producer.send(message);}
 		producer.close();
 
 	}
